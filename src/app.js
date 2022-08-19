0c706a55ccfe44c597aed829278aa094542ff9cc
macarenagimenez <107077871+macarenagimenez@users.noreply.github.com>
2022-08-19 15:23:33 -0300

    js working good

--- src/app.js
+++ src/app.js
@@ -1,11 +1,3 @@
-function init() {
-  let city = "London";
-
-  searchCityWeather(city, apiKey);
-  let form = document.querySelector("#inputCity");
-  form.addEventListener("submit", searchCity);
-}
-
 function date() {
   let now = new Date();
   let days = [
@@ -26,20 +18,10 @@ function date() {
   currentTime.innerHTML = `${day}, ${hours}:${minutes}hr.`;
 }
 
-function displayWeather(response) {
-  console.log(response);
-  let temperature = Math.round(response.data.main.temp);
-  document.querySelector("#valueTemp").innerHTML = `${temperature}°`;
-  let description = response.data.weather[0].main;
-  document.querySelector("#description").innerHTML = `${description}`;
-  let humidity = response.data.main.humidity;
-  document.querySelector("#humidity").innerHTML = `${humidity}%`;
-  let wind = response.data.wind.speed;
-  document.querySelector("#wind").innerHTML = `${wind}`;
-  let icon = response.data.weather[0].icon;
-  document
-    .querySelector("#icon")
-    .setAttribute("src", `http://openweathermap.org/img/wn/${icon}@2x.png`);
+function searchCityWeather(city) {
+  let apiKey = "d9fa039441ee765f866a01bc611a5d61";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
 }
 
 function searchCity(event) {
@@ -47,12 +29,54 @@ function searchCity(event) {
   let city = document.querySelector("#valueCity").value;
   searchCityWeather(city);
 }
-function searchCityWeather(city) {
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayWeather);
+
+function displayWeather(response) {
+  let temperature = Math.round(response.data.main.temp);
+  let description = response.data.weather[0].main;
+  let humidity = response.data.main.humidity;
+  let wind = response.data.wind.speed;
+  let icon = response.data.weather[0].icon;
+
+  celsiusTemperature = response.data.main.temp;
+
+  console.log(response);
+
+  document.querySelector("#valueTemp").innerHTML = `${temperature}`;
+  document.querySelector("#description").innerHTML = `${description}`;
+  document.querySelector("#humidity").innerHTML = `${humidity}%`;
+  document.querySelector("#wind").innerHTML = `${wind}`;
+  document
+    .querySelector("#icon")
+    .setAttribute("src", `http://openweathermap.org/img/wn/${icon}@2x.png`);
 }
 
-let apiKey = "d9fa039441ee765f866a01bc611a5d61";
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let fahrenheitTemperature = Math.round((celsiusTemperature * 9) / 5 + 32);
+  let temperatureElement = document.querySelector("#valueTemp");
+  temperatureElement.innerHTML = `${fahrenheitTemperature}`;
+  celsiuslink.classList.remove("active");
+  fahrenheitlink.classList.add("active");
+}
+
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#valueTemp");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  celsiuslink.classList.add("active");
+  fahrenheitlink.classList.remove("active");
+}
 
 date();
-init();
+searchCityWeather("London");
+
+let celsiusTemperature = null;
+
+let form = document.querySelector("#inputCity");
+form.addEventListener("submit", searchCity);
+
+let fahrenheitlink = document.querySelector("#fahrenheit-link");
+fahrenheitlink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiuslink = document.querySelector("#celsius-link");
+celsiuslink.addEventListener("click", displayCelsiusTemperature);
